3862dce482146a5ea77dfa22d83c41bd693fd19a
wangyun <519245844@qq.com>
2017-11-07 10:48:23 +0800

    test

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = MainTabBarViewController()
         window?.makeKeyAndVisible()
         
+        
         return true
     }
 

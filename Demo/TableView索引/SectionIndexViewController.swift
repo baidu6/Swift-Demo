9c4ac500132607201ed4ae1e9d57b9846705cb47
wangyun <519245844@qq.com>
2017-12-25 10:23:59 +0800

    添加自定义索引

---
+++ Demo/TableView索引/SectionIndexViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SectionIndexViewController.swift
+//  Demo
+//
+//  Created by 王云 on 2017/12/25.
+//  Copyright © 2017年 王云. All rights reserved.
+//
+
+import UIKit
+
+class SectionIndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.white
+    }
+
+    
+
+}
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+
 import SnapKit
+
+typealias  DictionaryDefault = Dictionary<String, Any>
+
+let ScreenWidth = UIScreen.main.bounds.size.width
+let ScreenHeight = UIScreen.main.bounds.size.height
 
 class ViewController: UIViewController {
     
     var tableView: BaseTableView!
-    var namesArray = ["CoreData", "Realm", "RxSwift", "SwiftForms", "AnimationNumbers", "引用类型和值类型","GCDTest", "Set(集合)Test", "函数闭包练习", "3DTouch(UITableView)", "UI预加载动画"]
+    var namesArray = ["CoreData", "Realm", "RxSwift", "SwiftForms", "AnimationNumbers", "引用类型和值类型","GCDTest", "Set(集合)Test", "函数闭包练习", "3DTouch(UITableView)", "UI预加载动画", "自定义TableView索引"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +30,9 @@ class ViewController: UIViewController {
 
     func setupTableView() {
         tableView = BaseTableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DemoCellID")
+        tableView.estimatedRowHeight = ScreenHeight
+//        tableView.register(UITableViewCellText.self, forCellReuseIdentifier: "DemoCellID")
+        tableView.register(UITableViewCellDetail.self, forCellReuseIdentifier: "DemoCellID")
         tableView.delegate = self
         tableView.dataSource = self
         view.addSubview(tableView)
@@ -53,14 +61,6 @@ class ViewController: UIViewController {
         tableView.tableFooterView = footerView
         
     }
-    
-    override func viewSafeAreaInsetsDidChange() {
-        super.viewSafeAreaInsetsDidChange()
-        
-        //IOS11新特性
-        print(view.safeAreaInsets)
-        
-    }
 
 }
 
@@ -72,9 +72,10 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return namesArray.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DemoCellID")
-        cell?.textLabel?.text = namesArray[indexPath.row]
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DemoCellID") as! UITableViewCellDetail
+        cell.displayData(dict: ["title": namesArray[indexPath.row], "detail": "点击查看详情----"])
+//        cell.displayData(title: namesArray[indexPath.row])
+        return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -123,7 +124,146 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
             let vc = PreviewLoadingViewController()
             vc.hidesBottomBarWhenPushed = true
             self.navigationController?.pushViewController(vc, animated: true)
+        }else if indexPath.row == 11 {
+            let vc = SectionIndexViewController()
+            vc.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(vc, animated: true)
         }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
+
+class BaseTableViewCell: UITableViewCell {
+    
+    var indexPath: IndexPath?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        selectionStyle = .none
+        contentView.backgroundColor = UIColor.white
+    }
+    
+    //MARK:- 隐藏分割线
+    func hiddenSeparator() {
+        separatorInset = UIEdgeInsetsMake(0, ScreenWidth, 0, 0)
+    }
+    
+    //MARK:- 去除左边分割线的间距
+    func zeroSeparator() {
+        separatorInset = UIEdgeInsets.zero
+    }
+    
+    //MARK:- 隐藏分割线后自己添加分割线
+    func addSeparatorZero() {
+        hiddenSeparator()
+        addLineBottom(edgeInsets: UIEdgeInsets.zero)
+    }
+    
+    //MARK:- 隐藏分割线后自己添加（默认有15的左边间距）
+    func addSeparatorDefault() {
+        hiddenSeparator()
+        addLineBottom(edgeInsets: UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0))
+    }
+    
+    func displayData(index: IndexPath, data: DictionaryDefault?) {
+        self.indexPath = index
+    }
+    
+}
+
+class UITableViewCellText: BaseTableViewCell {
+    fileprivate var titleLabel: UILabel!
+    override func setupUI() {
+        super.setupUI()
+        
+        addSeparatorZero()
+        
+        titleLabel = UILabel()
+        titleLabel.font = UIFont.systemFont(ofSize: 15)
+        titleLabel.textColor = UIColor.black
+        titleLabel.textAlignment = .left
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-16)
+            make.top.bottom.equalToSuperview()
+        }
+    }
+    
+    func displayData(title: String?) {
+        titleLabel.text = title
+    }
+}
+
+class UITableViewCellDetail: BaseTableViewCell {
+    
+    fileprivate var titleLabel: UILabel!
+    fileprivate var descLabel: UILabel!
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        titleLabel = UILabel()
+        titleLabel.font = UIFont.systemFont(ofSize: 16)
+        titleLabel.textAlignment = .left
+        titleLabel.textColor = UIColor.black
+        contentView.addSubview(titleLabel)
+        
+        descLabel = UILabel()
+        descLabel.font = UIFont.systemFont(ofSize: 14)
+        descLabel.textAlignment = .left
+        descLabel.textColor = UIColor.lightGray
+        contentView.addSubview(descLabel)
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(10)
+            make.left.equalToSuperview().offset(10)
+            make.right.equalToSuperview().offset(-10)
+        }
+        
+        descLabel.snp.makeConstraints { (make) in
+            make.left.right.equalTo(titleLabel)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.bottom.equalToSuperview().offset(-10)
+        }
+    }
+    
+    //MARK:- 展示数据
+    func displayData(dict: [String: Any]) {
+        titleLabel.text = dict["title"] as? String
+        descLabel.text = dict["detail"] as? String
+     }
+}
+
+extension UIView {
+    
+    @discardableResult
+    func addLineBottom(edgeInsets: UIEdgeInsets) -> UIView {
+        return addLine(edgeInsets: edgeInsets)
+    }
+    
+    func addLine(color: UIColor = UIColor.lightGray, height: CGFloat = 0.5, edgeInsets: UIEdgeInsets? = nil) -> UIView {
+        let view = UIView()
+        view.backgroundColor = color
+        addSubview(view)
+        
+        view.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview().offset(-(edgeInsets?.bottom ?? 0))
+            make.left.equalToSuperview().offset(edgeInsets?.left ?? 0)
+            make.right.equalToSuperview().offset(-(edgeInsets?.right ?? 0))
+            make.height.equalTo(height)
+        }
+        return view
     }
 }
 
